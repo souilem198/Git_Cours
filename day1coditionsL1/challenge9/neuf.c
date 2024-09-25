b652a51ab1227e95acb7071edba42d8409106547
souilem198 <souilem.mariem@gmail.com>
2024-09-25 18:04:05 +0200

    modification

---
+++ day1coditionsL1/challenge9/neuf.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+   int moyenne;
+   printf("veuillez entrer votre moyenne :");
+   scanf("%d",&moyenne);
+   if(moyenne<=10)
+    printf("recale");
+   else if(moyenne>=10 &&moyenne <=12)
+    printf("passable");
+   else if(moyenne>=12 &&moyenne <=14)
+    printf("assez bien");
+   else if(moyenne>=14 &&moyenne <=16)
+    printf("bien");
+   else
+    printf("tres bien");
+
+
+
+    return 0;
+}
